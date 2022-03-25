3d8c4859aa728d0c39fe4aead6186fb841da581e
AnodMohamed <96368212+AnodMohamed@users.noreply.github.com>
2022-03-25 13:32:09 +0300

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'admin.add-category-component' => 'App\\Http\\Livewire\\Admin\\AddCategoryComponent',
+  'admin.add-driver-component' => 'App\\Http\\Livewire\\Admin\\AddDriverComponent',
+  'admin.add-product-component' => 'App\\Http\\Livewire\\Admin\\AddProductComponent',
+  'admin.admin-dashboard-component' => 'App\\Http\\Livewire\\Admin\\AdminDashboardComponent',
+  'admin.admin-view-product-component' => 'App\\Http\\Livewire\\Admin\\AdminViewProductComponent',
+  'admin.manage-category-component' => 'App\\Http\\Livewire\\Admin\\ManageCategoryComponent',
+  'admin.manage-drivers-component' => 'App\\Http\\Livewire\\Admin\\ManageDriversComponent',
+  'admin.manage-product-component' => 'App\\Http\\Livewire\\Admin\\ManageProductComponent',
+  'admin.manage-users-component' => 'App\\Http\\Livewire\\Admin\\ManageUsersComponent',
+  'driver.driver-dashboard-component' => 'App\\Http\\Livewire\\Driver\\DriverDashboardComponent',
+  'home-component' => 'App\\Http\\Livewire\\HomeComponent',
+  'test2' => 'App\\Http\\Livewire\\Test2',
+  'user.user-dashboard-component' => 'App\\Http\\Livewire\\User\\UserDashboardComponent',
+);
